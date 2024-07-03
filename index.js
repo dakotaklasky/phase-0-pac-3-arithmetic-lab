696a9e798e6ee579961ef0c60cc38562b3d5caa0
Dakota <dakota.klasky@gmail.com>
2024-07-03 13:07:54 -0400

    create functions

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(a,b) {
+    return a+b;
+  }
+
+function subtract(a,b) {
+    return a-b;
+  }
+
+function multiply(a,b) {
+    return a*b;
+  }
+
+function divide(a,b) {
+    return a/b;
+  }
+
+function increment(n) {
+    return n+1;
+  }
+
+function decrement(n) {
+    return n-1;
+  }
+
+function makeInt(s) {
+    return parseInt(s,10);
+  }
+
+function preserveDecimal(s) {
+    return parseFloat(s);
+  }
